53c4c9e0c9c716ca7c1271608168d94426e41549
Kartik Sharma <kartik.sharma4@cognizant.com>
2020-11-26 09:48:26 +0530

    Project file updated

--- JavaExercise1-26-11-20-Morning(Comparable-Comparator-AnonymousClass)/src/comparator/SortMain.java
+++ JavaExercise1-26-11-20-Morning(Comparable-Comparator-AnonymousClass)/src/comparator/SortMain.java
@@ -6,38 +6,45 @@ import java.util.Comparator;
 import java.util.List;
 
 public class SortMain {
-	
-	public static void main(String...strings) {
-List<Shop> list = new ArrayList<Shop>();
-List<Shop> list1 = new ArrayList<Shop>();
-	list.add(new Shop(1,"Soap",100.0));
-	list.add(new Shop(3,"Shampoo",300.0));
-	list.add(new Shop(2,"Conditioner",200.0));
-	
-	list1.add(new Shop(1,"Soap",100.0));
-	list1.add(new Shop(3,"Shampoo",300.0));
-	list1.add(new Shop(2,"Conditioner",200.0));
-	
-	System.out.println("Comparable");
-	//Using comparable	
-	Collections.sort(list);
-	System.out.println(list);
-	
-	System.out.println("Using Comparator");
-	//Using comparator
-	Comparator<Shop> c = new Comparator<Shop>() {
-		@Override
-		public int compare(Shop o1, Shop o2) {
-			return o1.getId()-o2.getId();
-		}
-	};
-	
-	Collections.sort(list1, c);
-	System.out.println(list1);
-	
-	
+
+	public static void main(String... strings) {
+		List<Shop> list = new ArrayList<Shop>();
+		List<Shop> list1 = new ArrayList<Shop>();
+		list.add(new Shop(1, "Soap", 100.0));
+		list.add(new Shop(3, "Shampoo", 300.0));
+		list.add(new Shop(2, "Conditioner", 200.0));
+
+		list1.add(new Shop(1, "Soap", 100.0));
+		list1.add(new Shop(3, "Shampoo", 300.0));
+		list1.add(new Shop(2, "Conditioner", 200.0));
+
+		// Using comparable
+		System.out.println("Using Comparable");
+		Collections.sort(list);
+		System.out.println(list);
+
+		// Using comparator
+		System.out.println("Using Comparator -  Ascending");
+		Comparator<Shop> c = new Comparator<Shop>() {
+			@Override
+			public int compare(Shop o1, Shop o2) {
+				return o1.getId() - o2.getId();
+			}
+		};
+		Collections.sort(list1, c);
+		System.out.println(list1);
+
+		// Using comparator
+		System.out.println("Using Comparator -  Descending");
+		Comparator<Shop> c1 = new Comparator<Shop>() {
+			@Override
+			public int compare(Shop o1, Shop o2) {
+				return o2.getId() - o1.getId();
+			}
+		};
+		Collections.sort(list1, c1);
+		System.out.println(list1);
+
 	}
 
-	
-	
 }
