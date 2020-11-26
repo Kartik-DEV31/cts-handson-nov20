e62b8355489c73b846955043cea615a7f0cd8101
Kartik Sharma <kartik.sharma4@cognizant.com>
2020-11-26 12:29:27 +0530

    Employee Menu Project Added

--- JavaExcercise2-26-11-20-Morning(EmployeeMenu)/src/employeeMenu/EmployeeService.java
+++ JavaExcercise2-26-11-20-Morning(EmployeeMenu)/src/employeeMenu/EmployeeService.java
@@ -3,14 +3,17 @@ package employeeMenu;
 import java.util.List;
 
 public interface EmployeeService {
-		
+
 	void add(Integer id, String dob, String name, Double salary);
+
 	List findEmployeeById(Integer id);
+
 	List getEmployees();
+
 	List getSortedEmployeesById();
+
 	List getSortedEmployeesBySalary();
+
 	List getSortedEmployeesByName();
 
-	
-	
 }
--- JavaExcercise2-26-11-20-Morning(EmployeeMenu)/src/employeeMenu/EmployeeServiceImpl.java
+++ JavaExcercise2-26-11-20-Morning(EmployeeMenu)/src/employeeMenu/EmployeeServiceImpl.java
@@ -7,46 +7,36 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class EmployeeServiceImpl implements EmployeeService{
+public class EmployeeServiceImpl implements EmployeeService {
 
-	
 	List<Employee> list = new ArrayList<Employee>();
-	
-	
+
 	@Override
 	public void add(Integer id, String dob, String name, Double salary) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-		LocalDate date = LocalDate.parse(dob,formatter);
+		LocalDate date = LocalDate.parse(dob, formatter);
 		list.add(new Employee(id, date, name, salary));
-		
-		
-		
+
 	}
 
 	@Override
 	public List findEmployeeById(Integer id) {
 		List<Employee> list2 = new ArrayList<Employee>();
-		
-		
-		
-		for(int i =0;i<list.size();i++)
-		{
-			if(list.get(i).getId()==id)
-			{
+
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getId() == id) {
 				list2.add(list.get(i));
 			}
-			
+
 		}
 		return list2;
-		
-		
+
 	}
 
 	@Override
 	public List getEmployees() {
 
-		
-		return list;	
+		return list;
 	}
 
 	@Override
@@ -56,27 +46,26 @@ public class EmployeeServiceImpl implements EmployeeService{
 			@Override
 			public int compare(Employee o1, Employee o2) {
 				// TODO Auto-generated method stub
-				return o1.getId()-o2.getId();
+				return o1.getId() - o2.getId();
 			}
-			
-			
+
 		};
-		
+
 		Collections.sort(list, c);
 		return list;
-		
+
 	}
 
 	@Override
 	public List getSortedEmployeesBySalary() {
-	
-		Collections.sort(list, (e1,e2) -> e1.getSalary().compareTo(e2.getSalary()) );
+
+		Collections.sort(list, (e1, e2) -> e1.getSalary().compareTo(e2.getSalary()));
 		return list;
 	}
 
 	@Override
 	public List getSortedEmployeesByName() {
-		Collections.sort(list, (e1,e2) -> e1.getName().compareTo(e2.getName()) );
+		Collections.sort(list, (e1, e2) -> e1.getName().compareTo(e2.getName()));
 		return list;
 
 	}
